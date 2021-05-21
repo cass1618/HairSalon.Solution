28974a521d3ebe77234fdf798e9568e1fde7dfd8
cass1618 <cass16180@gmail.com>
2021-05-21 09:53:59 -0700

    :sparkles: Create Client class

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<string> ServicesReceived { get; set; }
+    public DateTime NextAppointment { get; set; }
+    public int CompletedAppointments { get; set; }
+    public int StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
+  }
+}
